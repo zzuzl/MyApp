0fd93cdabc783560c5b69cb263809014c742b013
zhanglei <672399171@qq.com>
2018-11-17 20:54:39 +0800

    checkLogin

--- src/main/java/cn/zlihj/web/IndexController.java
+++ src/main/java/cn/zlihj/web/IndexController.java
@@ -1,8 +1,10 @@
 package cn.zlihj.web;
 
+import cn.zlihj.dao.WorkTypeDao;
 import cn.zlihj.domain.Company;
 import cn.zlihj.domain.Project;
 import cn.zlihj.domain.Staff;
+import cn.zlihj.domain.WorkType;
 import cn.zlihj.dto.Result;
 import cn.zlihj.service.CompanyService;
 import cn.zlihj.service.ProjectService;
@@ -15,6 +17,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
@@ -23,6 +27,8 @@ public class IndexController {
     private CompanyService companyService;
     @Autowired
     private ProjectService projectService;
+    @Autowired
+    private WorkTypeDao workTypeDao;
 
     @RequestMapping("/index")
     public String index(Model model) {
@@ -34,6 +40,14 @@ public class IndexController {
     public Result checkLogin() {
         Result result = Result.successResult();
         Staff staff = LoginContext.currentUser();
+        List<WorkType> workTypes = workTypeDao.list();
+        for (WorkType workType : workTypes) {
+            if (workType.getId().equals(staff.getType().getId())) {
+                staff.setType(workType);
+                break;
+            }
+        }
+
         switch (staff.getSource()) {
             case COMPANY:
                 Company company = companyService.findById(staff.getPid());
