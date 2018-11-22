a57a038ec342809d5dce87d97c48b81b837153c8
zhanglei <672399171@qq.com>
2018-11-22 22:09:49 +0800

    checkLogin

--- src/main/java/cn/zlihj/web/IndexController.java
+++ src/main/java/cn/zlihj/web/IndexController.java
@@ -7,12 +7,15 @@ import cn.zlihj.dto.Result;
 import cn.zlihj.service.CompanyService;
 import cn.zlihj.service.ProjectService;
 import cn.zlihj.service.StaffService;
+import cn.zlihj.util.ExcelUtil;
 import cn.zlihj.util.LoginContext;
+import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -66,14 +70,25 @@ public class IndexController {
         Result result = null;
         try {
             if (!file.isEmpty()) {
+                if (!FilenameUtils.isExtension(file.getOriginalFilename(), "xls")) {
+                    throw new RuntimeException("仅支持.xls文件");
+                }
+
                 String rootPath = "/tmp";
                 File dir = new File(rootPath + File.separator + "tmpFiles");
                 if (!dir.exists()) {
-                    dir.mkdirs();
+                    Assert.isTrue(dir.mkdirs(), "文件夹创建失败");
                 }
                 File serverFile = new File(dir.getAbsolutePath() + File.separator + file.getOriginalFilename());
                 file.transferTo(serverFile);
                 logger.info("You successfully uploaded file=" +  file.getOriginalFilename());
+                String[][] strings = ExcelUtil.readFromFile(serverFile, 14, 1000);
+                for (String[] string : strings) {
+                    logger.info(Arrays.toString(string));
+
+                }
+
+                serverFile.deleteOnExit();
             } else {
                 logger.error("You failed to upload " +  file.getOriginalFilename() + " because the file was empty.");
             }
